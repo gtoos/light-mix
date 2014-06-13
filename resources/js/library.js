9a4b46f2c86762c3ab6ffdd29b89c6a9a8307bac
gtoos <mail2g2.rdl@gmail.com>
2014-06-13 09:53:13 +0530

    added spotlight and linked it to sliders

--- resources/js/library.js
+++ resources/js/library.js
@@ -9,15 +9,89 @@ function initCanvas(canvasId, bgColor, opacity) {
 	canvas.style.opacity=opacity;
 }
 
-function initSlider(color) {
-	var slider = document.createElement('input');
-    slider.id = color+"Slider";
-    slider.type = 'range';
-    slider.min = 0;
-    slider.max = 100;
-    slider.value = 10;
-    slider.step = 1;
+function initStage() {
 
-    var canvas = document.getElementById("lightCanvas");
-    canvas.appendChild(slider); 
+}
+
+function initSpotlight() {
+	var canvas = document.getElementById("lightCanvas");
+	var canvasContext = canvas.getContext('2d');
+	var cX = 0;
+	var cY = 0;
+	var radius = 50;
+
+	//save state
+	canvasContext.save();
+
+	// Spotlight Position
+	sX = canvas.width/2;
+	sY = canvas.height/2 + 200;
+
+	//translate context
+	canvasContext.translate(sX, sY);
+
+	//scale context horizontally
+	canvasContext.scale(2, 1);
+
+	//draw circle which will be stretched to an oval
+	canvasContext.beginPath();
+	canvasContext.arc(cX, cY, radius, 0, 2*Math.PI, false);
+
+	// restore to original state
+	canvasContext.restore();
+
+	//apply styling
+	canvasContext.fillStyle = '#8ED6FF';
+	canvasContext.fill();
+	canvasContext.lineWidth = 5;
+	canvasContext.strokeStyle = 'black';
+	canvasContext.stroke();
+}
+
+function updateSpotlight(r, g, b) {
+	var canvas = document.getElementById("lightCanvas");
+	var canvasContext = canvas.getContext('2d');
+	var cX = 0;
+	var cY = 0;
+	var radius = 50;
+
+	//save state
+	canvasContext.save();
+
+	// Spotlight Position
+	sX = canvas.width/2;
+	sY = canvas.height/2 + 200;
+
+	//translate context
+	canvasContext.translate(sX, sY);
+
+	//scale context horizontally
+	canvasContext.scale(2, 1);
+
+	//draw circle which will be stretched to an oval
+	canvasContext.beginPath();
+	canvasContext.arc(cX, cY, radius, 0, 2*Math.PI, false);
+
+	// restore to original state
+	canvasContext.restore();
+
+	//apply styling
+	canvasContext.fillStyle = "rgb(" + r + "," + g + "," + b +")";
+	canvasContext.fill();
+	canvasContext.lineWidth = 5;
+	canvasContext.strokeStyle = 'black';
+	canvasContext.stroke();
+}
+
+function draw() {
+
+	var scale = 255/100;
+	var r = parseInt($("input[name=redSlider]").val() * scale);
+	var b = parseInt($("input[name=blueSlider]").val() * scale);
+	var g = parseInt($("input[name=greenSlider]").val() * scale);
+
+	console.log("r = " + r); 	
+	console.log("g = " + g); 	
+	console.log("b = " + b); 	
+	updateSpotlight(r, g, b);
 }
